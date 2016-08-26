e414d4a990b503145050cc74f536f01a87f6f007
Troy Ault <aultt@hotmail.com>
2016-08-26 16:25:18 -0400

    Addition of Configuration for SQLDataStore

--- scripts/InputDSCConfigExamples.ps1
+++ scripts/InputDSCConfigExamples.ps1
@@ -1,60 +1,89 @@
 ﻿#Set Required Variables
-$OutputPath = 'G:\DSC_Mof'     #Location where mof files will be stored
-$CentralDataStore = 'OHSQL9035' #Central location where metadata for DSC configurations are/will be stored
+$OutputPath = 'C:\DSC_Mof'     #Location where mof files will be stored
+$CentralDataStore = 'OHSQL9037' #Central location where metadata for DSC configurations are/will be stored
+$ConfigurationHelperPath = 'C:\DSC-data-driven-deployment\'
+$SQLServerBitsLocation = '\\ohdc9000\SQLAutoBuilds'
+$DefaultSQLVersion = 'SQL2014'
+$WindowsBitsLocation = '\\ohdc9000\SQLAutoBuilds'
+$PSDscAllowDomainUser = $true 
+$PSDscAllowPlainTextPassword = $true #Should be $false for production workloads
+$DefaultParentConfigurationName = 'CompanyDefault'
+$DefaultSQLConfigurationName = 'SQLDefault'
+$SQLAdminAccount = 'CORP\Administrator'
+$SQLInstanceName = 'MSSQLSERVER'
+$SQLInstallShareDir = 'C:\Program Files\Microsoft SQL Server'
+$SQLUserDBDir = 'C:\Program Files\Microsoft SQL Server\Data'
+$SQLTempDBLogDir = 'C:\Program Files\Microsoft SQL Server\Data'
+$SQLTempDBDir = 'C:\Program Files\Microsoft SQL Server\Data'
+$InstallSQLDataDir = 'C:\Program Files\Microsoft SQL Server\Data'
+$SQLUserDBLogDir = 'C:\Program Files\Microsoft SQL Server\Data'
+$InstallSharedWOWDir = 'c:\Program Files (x86)\Microsoft SQL Server'
+$Features = 'SQLENGINE,IS,SSMS,ADV_SSMS'
+$SQLBackupDir = 'C:\Program Files\Microsoft SQL Server\Backup' 
+$InstanceDir = 'C:\Program Files\Microsoft SQL Server'
+$DMaxDop = $true
+$MaxDopVal = '0'
+$DMemory = $true
+$MinMemory = '256'
+$MaxMemory = '512'
+$InstallerAccountName = 'SQLInstallerAccount'
+$SQLAgentAccountName = 'SQLAgentAccount'
+$SQLServiceAccountName = 'SQLServiceAccount'
+$SQLInstallerAccount = 'CORP\SQLAuto'
+$SQLServiceAccount ='CORP\SQLSvc'
+$SQLAgentAccount = 'CORP\SQLAgt' 
+#$NodestoConfigure = 'ohsql9037'
 
 # Load Common Code
-Import-Module -Name "G:\DSC-data-driven-deployment\Modules\ConfigurationHelper.psm1" -Verbose:$False -ErrorAction Stop
+Import-Module -Name $("$ConfigurationHelperPath\Modules\ConfigurationHelper.psm1") -Verbose:$False -ErrorAction Stop
 
 #Add Default build for Company.  This maybe SQL2014 on Windows 2012R2. 
 #The ability to add your Own is still there but if you drop items on the queue and dont specify the default will be leveraged
-$ParentPayload = [PSCustomObject]@{SourcePath =  "\\ohdc9000\SQLAutoBuilds\SQL2014"
-                                    NETPath    =  "\\ohdc9000\SQLAutoBuilds\WIN2012R2\Sxs"
-                                    PSDscAllowDomainUser = "True"
-                                    PSDscAllowPlainTextPassword = "True"
+$ParentPayload = [PSCustomObject]@{SourcePath =  $("$SQLServerBitsLocation\$DefaultSQLVersion")
+                                    NETPath    =  $("$WindowsBitsLocation\$DefaultSQLVersion")
+                                    PSDscAllowDomainUser = $PSDscAllowDomainUser
+                                    PSDscAllowPlainTextPassword = $PSDscAllowPlainTextPassword
                                    }
-Add-NewParentConfiguration -ParentConfigurationName "CompanyDefault" -Payload $ParentPayload -ScriptName "DSCStandAloneJSON_CalledbyDSCExecution.ps1" -ScriptPath "G:\DSC-data-driven-deployment\scripts\" -SQLServer $CentralDataStore 
+Add-NewParentConfiguration -ParentConfigurationName $DefaultParentConfigurationName -Payload $ParentPayload -ScriptName "DSCSQLMetaBuild.ps1" -ScriptPath $("$ConfigurationHelperPath\scripts\") -SQLServer $CentralDataStore 
 
 #Create New Default configuration which Nodes can be assigned to
 #Allows to call Add-NewConfigurationToQueue to be utilized to Enqueue new configurations with an existing default
-$SQLPayLoad = [PSCustomObject]@{AdminAccount         =         "CORP\Troyault"; 
-                                     InstanceName         =         "MSSQLSERVER";                                                                                                                                                                                                           
-                                     InstallSharedDir     =         "T:\Program Files\Microsoft SQL Server";                                                                                                                                                                                 
-                                     SQLUserDBDir         =         "G:\MSSQL\Data";                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
-                                     SQLTempDBLogDir      =         "T:\MSSQL\Data";                                                                                                                                                                                                                                                                                                                                                                                    
-                                     SQLTempDBDir         =         "T:\MSSQL\Data";                                                                                                                                                                                                         
-                                     InstallSQLDataDir    =         "G:\MSSQL\Data";                                                                                                                                                                                                                                                                                                                                                                                                                    
-                                     SQLUserDBLogDir      =         "L:\MSSQL\Data";                                                                                                                                                                                                         
-                                     InstallSharedWOWDir  =         "T:\Program Files (x86)\Microsoft SQL Server";                                                                                                                                                                           
-                                     Features             =         "SQLENGINE,IS,SSMS,ADV_SSMS";                                                                                                                                                                                            
-                                     SQLBackupDir         =         "G:\MSSQL\Backup";                                                                                                                                                                                                       
-                                     InstanceDir          =         "T:\Program Files\Microsoft SQL Server";                                                                                                                                                                                 
-                                     DMaxDop              =         "True";                                                                                                                                                                                                                  
-                                     MaxDopVal            =         "0";                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
-                                     DMemory              =         "True"
-                                     MinMemory            =         "256";                                                                                                                                                                                                          
-                                     MaxMemory            =         "512"}
-Add-NewNodeConfigurationDefault -NodeConfigurationName "DefaultSQL" -Payload $SQLPayLoad -SQLServer $CentralDataStore
+$SQLPayLoad = [PSCustomObject]@{AdminAccount         =         $SQLAdminAccount; 
+                                     InstanceName         =         $SQLInstanceName;                                                                                                                                                                                                           
+                                     InstallSharedDir     =         $SQLInstallShareDir;                                                                                                                                                                                 
+                                     SQLUserDBDir         =         $SQLUserDBDir;                                                    
+                                     SQLTempDBLogDir      =         $SQLTempDBLogDir;                                                                                                             
+                                     SQLTempDBDir         =         $SQLTempDBDir;                                                                                                                                                                                                         
+                                     InstallSQLDataDir    =         $InstallSQLDataDir;                                                                                                                                                
+                                     SQLUserDBLogDir      =         $SQLUserDBLogDir;                                                                                                                                                                                                         
+                                     InstallSharedWOWDir  =         $InstallSharedWOWDir;                                                                                                                                                                           
+                                     Features             =         $Features;                                                                                                                                                                                            
+                                     SQLBackupDir         =         $SQLBackupDir;                                                                                                                                                                                                       
+                                     InstanceDir          =         $InstanceDir;                                                                                                                                                                                 
+                                     DMaxDop              =         $DMaxDop;                                                                                                                                                                                                                  
+                                     MaxDopVal            =         $MaxDopVal;                                                                                                                                                                                                                             DMemory              =         $DMemory
+                                     MinMemory            =         $MinMemory;                                                                                                                                                                                                          
+                                     MaxMemory            =         $MaxMemory}
+Add-NewNodeConfigurationDefault -NodeConfigurationName $DefaultSQLConfigurationName -Payload $SQLPayLoad -SQLServer $CentralDataStore
 
 #Adds Credential CORP\MyCred to the Credential Table with a secure password.  Only this user and machine combo can retrieve.
 #ASSUMPTION:  Running from the machine which will call back to retrieve credential and run configuration
 #User will be prompted for the Password
-Add-PSCredential -CredentialName "SQLInstallerAccount"  -Credential "CORP\SQLAuto" -SQLServer $CentralDataStore
-Add-PSCredential -CredentialName "SQLAgentAccount"  -Credential "CORP\SQLAgt" -SQLServer $CentralDataStore
-Add-PSCredential -CredentialName "SQLServiceAccount"  -Credential "CORP\SQLSvc" -SQLServer $CentralDataStore
+Add-PSCredential -CredentialName $InstallerAccountName -Credential $SQLInstallerAccount -SQLServer $CentralDataStore
+Add-PSCredential -CredentialName $SQLAgentAccountName  -Credential $SQLAgentAccount  -SQLServer $CentralDataStore
+Add-PSCredential -CredentialName $SQLServiceAccountName -Credential $SQLServiceAccount -SQLServer $CentralDataStore
 
 #Add Reference of Installer Credential to Default Parent Configuration
 #Add Reference of Credentials for DefaultSQL Configuration
 #Add-NewNodeConfigurationCredential -NodeConfigurationName 
-Add-NewParentConfigurationCredential -ParentConfigurationName "CompanyDefault" -CredentialName "SQLInstallerAccount" -Verbose
+Add-NewParentConfigurationCredential -ParentConfigurationName $DefaultParentConfigurationName -CredentialName $InstallerAccountName -Verbose
 
 #Utilize the below to add (N) number of servers to an existing configuration StandAloneSQL 
 #INFO:  This is an async call to add work to queue.  When and how often queue is drained is configured by scheduling next call.
 #TODO:  Need to do some validation here to ensure the configuration they are requesting actually exists
-$Config = [PSCustomObject]@{Configuration ="CompanyDefault"
-                            NodeName ='ohsql9030'}
+$Config = [PSCustomObject]@{Configuration =$DefaultParentConfigurationName
+                            NodeName =$NodestoConfigure}
 Add-NewConfigurationToQueue -Configuration $Config -SQLServer $CentralDataStore 
-
-
 
 ##########################################################################################################
 #Below are additional cmdlets which are available but not needed for Setup
