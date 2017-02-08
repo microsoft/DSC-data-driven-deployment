6dcc9bae2fb00e57bfdcf8eecb16afeddef84779
Troy Ault <troyault@microsoft.com>
2017-02-08 14:04:56 -0500

    Removal of Test Scripts not needed

--- utility/LabInaBox/Scripts/Copyfilefromsession.ps1
+++
@@ -1,4 +0,0 @@
-﻿$Cred = Get-Credential
-$Session = New-PSSession -VMName DD-Dev -Credential $Cred
-
-Copy-Item -Path c:\DSCCentral.dacpac -Destination D:\ -FromSession $Session
--- utility/LabInaBox/Scripts/Copyfiletosession.ps1
+++
@@ -1,5 +0,0 @@
-﻿$Cred = Get-Credential
-$Session = New-PSSession -VMName DD-DC -Credential $Cred
-
-$Session
-Copy-Item -Path D:\Users\TroyAult\Downloads\SSMS-Setup-ENU.exe -Destination c:\ -ToSession $Session
