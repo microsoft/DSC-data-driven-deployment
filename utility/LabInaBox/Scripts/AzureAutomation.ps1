f82a37340689f850ec1c411770212bd8aa7fe0da
Troy Ault <troyault@microsoft.com>
2017-08-17 14:45:16 -0400

    Updates to Azure Labinabox

---
+++ utility/LabInaBox/Scripts/AzureAutomation.ps1
@@ -0,0 +1,50 @@
+﻿Add-AzureRmAccount
+$AutoAccount = 'LIABAuto'
+$AutoRG = 'LIAB_RG'
+$cred = Get-Credential 
+
+# Define the parameters for Get-AzureRmAutomationDscOnboardingMetaconfig using PowerShell Splatting
+$Params = @{
+
+    ResourceGroupName = 'LIAB_RG'; # The name of the ARM Resource Group that contains your Azure Automation Account
+    AutomationAccountName = 'LIABAuto'; # The name of the Azure Automation Account where you want a node on-boarded to
+    ComputerName = @('AAL-SQL2'); # The names of the computers that the meta configuration will be generated for
+    OutputFolder = "E:\";
+}
+
+# Use PowerShell splatting to pass parameters to the Azure Automation cmdlet being invoked
+Get-AzureRmAutomationDscOnboardingMetaconfig @Params -Force
+Set-DscLocalConfigurationManager -Path E:\DscMetaConfigs -Credential $cred
+
+#Modifies the node configuration that a DSC node is mapped to.
+$MyNode=Get-AzureRMAutomationDscNode -ResourceGroupName $AutoRG -AutomationAccountName $AutoAccount -Name 'AAL-SQL2'
+Set-AzureRmAutomationDscNode -NodeConfigurationName 'TestConfig.WebServer' -ResourceGroupName $AutoRG -AutomationAccountName $AutoAccount -Id $MyNode.Id
+
+#Remove Node from Azure Automation Control
+Unregister-AzureRmAutomationDscNode -Id $MyNode.Id -ResourceGroupName $AutoRG -AutomationAccountName $AutoAccount -Force
+
+#Registers Azure VM as a DSC Node
+Register-AzureRMAutomationDscNode
+
+#Get-AzureRmAutomationAccount -Name $AutoAccount -ResourceGroupName $AutoRG
+
+New-AzureRMAutomationCredential -AutomationAccountName $AutoAccount -ResourceGroupName $AutoRG -Name 'AALAdministrator' -Description 'AAL Domain Administrator Account' -Value $cred
+
+
+$myCredential = Get-AzureRMAutomationCredential -Name 'AALAdministrator2' -ResourceGroupName $AutoRG -AutomationAccountName $AutoAccount
+$userName = $myCredential.UserName
+$securePassword = $myCredential.Password
+$password = $myCredential.GetNetworkCredential().Password
+
+
+get-azurermvm | Stop-AzureRmVM
+
+$cred
+$myCredential.UserName
+
+
+
+
+$myCredential = Get-AzureRMAutomationCredential -Name 'AALAdministrator2' -ResourceGroupName $AutoRG -AutomationAccountName $AutoAccount
+$myCredential | gm
+$myCredential
---
+++ utility/LabInaBox/Scripts/AzureAutomationAG.ps1
@@ -0,0 +1,47 @@
+﻿
+#################################################################################################################
+#Availability Groups
+#################################################################################################################
+#Import the tools to convert Configuration to HashTable
+Import-Module "C:\Program Files\WindowsPowerShell\Modules\DSC-data-driven-deployment\modules\ConfigurationHelper.psm1"
+
+
+$RG = 'LIAB_RG'
+$Auto = 'LIABAuto'
+$Config = 'DefaultSQLOnPrem'
+$ConfigParms = $(Get-Content 'D:\LabInaBox\LabConfig\AAL\AAL_SQLConfigAO.json')| ConvertFrom-Json| Convert-PSObjectToHashtable
+
+$Params = @{
+
+     ResourceGroupName = $RG; # The name of the ARM Resource Group that contains your Azure Automation Account
+     AutomationAccountName = $Auto; # The name of the Azure Automation Account where you want a node on-boarded to
+     ComputerName = @('AAL-SQL4'); # The names of the computers that the meta configuration will be generated for
+     OutputFolder = "E:\";
+}
+
+# Use PowerShell splatting to pass parameters to the Azure Automation cmdlet being invoked
+Get-AzureRmAutomationDscOnboardingMetaconfig @Params -Force
+
+$cred =Get-Credential
+
+Set-DscLocalConfigurationManager -Path E:\DscMetaConfigs -Credential $cred -Force 
+
+
+$ConfigurationData =@{
+	AllNodes = @(
+		@{
+			NodeName = "SQLServer"         
+            PSDscAllowPlainTextPassword = $true
+            PSDscAllowDomainUser =$true
+		}
+	)
+}
+#Compile New Configuration
+$CompilationJob =  Start-AzureRmAutomationDscCompilationJob -ResourceGroupName $RG -AutomationAccountName $Auto -ConfigurationName 'DefaultAOOnPremPrimary' -Parameters $ConfigParms -ConfigurationData $ConfigurationData 
+$CompilationJob2 =  Start-AzureRmAutomationDscCompilationJob -ResourceGroupName $RG -AutomationAccountName $Auto -ConfigurationName 'DefaultAOOnPremSecondary' -Parameters $ConfigParms -ConfigurationData $ConfigurationData 
+
+
+$NodeIdPrimary = Get-AzureRmAutomationDscNode -ResourceGroupName $RG -AutomationAccountName $Auto | Where-Object NAME -EQ 'AAL-SQL1' | Select-Object Id
+$NodeIDSecondary = Get-AzureRmAutomationDscNode -ResourceGroupName $RG -AutomationAccountName $Auto | Where-Object NAME -EQ 'AAL-SQL2' | Select-Object Id
+Set-AzureRmAutomationDscNode -NodeConfigurationName 'DefaultAOOnPremPrimary.SQLServer' -ResourceGroupName $RG -AutomationAccountName $Auto -Id $NodeIdPrimary.Id
+Set-AzureRmAutomationDscNode -NodeConfigurationName 'DefaultAOOnPremSecondary.SQLServer' -ResourceGroupName $RG -AutomationAccountName $Auto -Id $NodeIDSecondary.Id
